913959988cb7287c62a8e6a212a4a46cbdccf49e
AmberVolt <184669028+Vanshgoyal5102007@users.noreply.github.com>
2026-05-23 16:33:51 +0530

    Build function getComputerChoice to get the computer's choice for the game

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function getComputerChoice(){
+    const randomNum = getRandomNumber(1,3);
+    let choiceComp;
+    switch (randomNum){
+        case 1:
+            return "Rock"
+        case 2:
+            return "Paper"
+        case 3:
+            return "Scissors";
+    }
+}

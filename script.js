be748c5ed6c375cdbf0797161dc2032945d4bd5b
AmberVolt <184669028+Vanshgoyal5102007@users.noreply.github.com>
2026-06-06 15:03:26 +0530

    Add DOM Methods to display playing buttons and game logs right on the screen

--- script.js
+++ script.js
@@ -23,45 +23,77 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
 
+    const result = document.createElement('p');
+
     if (computerChoice == humanChoice){
-        console.log("It's a tie!");
+        result.textContent = "It's a tie!";
     }
     else if((computerChoice == 'rock' && humanChoice == 'paper') || 
             (computerChoice == 'paper' && humanChoice == 'scissors') || 
             (computerChoice == 'scissors' && humanChoice == 'rock')){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
         humanScore++;
-    }
+        result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+        }
     else{
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
         computerScore++;
+        result.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`;
     }
 
+    score.textContent = `Current Score - You: ${humanScore} Computer: ${computerScore}`;
+    
+    resultDiv.appendChild(result);
+
 }
 
-function playGame(){
+// function playGame(){
 
-    const humanSelection1 = getHumanChoice();
-    const computerSelection1 = getComputerChoice();
-    playRound(humanSelection1, computerSelection1);
+//     const humanSelection1 = getHumanChoice();
+//     const computerSelection1 = getComputerChoice();
+//     playRound(humanSelection1, computerSelection1);
 
-    const humanSelection2 = getHumanChoice();
-    const computerSelection2 = getComputerChoice();
-    playRound(humanSelection2, computerSelection2);
+//     const humanSelection2 = getHumanChoice();
+//     const computerSelection2 = getComputerChoice();
+//     playRound(humanSelection2, computerSelection2);
 
-    const humanSelection3 = getHumanChoice();
-    const computerSelection3 = getComputerChoice();
-    playRound(humanSelection3, computerSelection3);
+//     const humanSelection3 = getHumanChoice();
+//     const computerSelection3 = getComputerChoice();
+//     playRound(humanSelection3, computerSelection3);
 
-    const humanSelection4 = getHumanChoice();
-    const computerSelection4 = getComputerChoice();
-    playRound(humanSelection4, computerSelection4);
+//     const humanSelection4 = getHumanChoice();
+//     const computerSelection4 = getComputerChoice();
+//     playRound(humanSelection4, computerSelection4);
 
-    const humanSelection5 = getHumanChoice();
-    const computerSelection5 = getComputerChoice();
-    playRound(humanSelection5, computerSelection5);
+//     const humanSelection5 = getHumanChoice();
+//     const computerSelection5 = getComputerChoice();
+//     playRound(humanSelection5, computerSelection5);
 
-    console.log(`Your score: ${humanScore} | Computer's Score: ${computerScore}`);
-}
+//     console.log(`Your score: ${humanScore} | Computer's Score: ${computerScore}`);
+// }
 
-playGame();
+// playGame();
+
+const rock = document.createElement('button');
+rock.textContent = 'Rock';
+const paper = document.createElement('button');
+paper.textContent = 'Paper';
+const scissors = document.createElement('button');
+scissors.textContent = 'Scissors';
+
+const body = document.querySelector('body');
+
+body.appendChild(rock);
+body.appendChild(paper);
+body.appendChild(scissors);
+
+const resultDiv = document.createElement('div');
+
+rock.addEventListener('click', () => playRound('rock', getComputerChoice()));
+paper.addEventListener('click', () => playRound('paper', getComputerChoice()));
+scissors.addEventListener('click', () => playRound('scissors', getComputerChoice()));
+
+const winner = document.createElement('p');
+const score = document.createElement('p');
+
+body.appendChild(resultDiv);
+
+body.appendChild(score);
